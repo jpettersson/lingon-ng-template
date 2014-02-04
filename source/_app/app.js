227ed47cc29a48b53870db49258262785faeed29
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-02-04 16:10:15 +0100

    Added a basic project structure

---
+++ source/_app/app.js
@@ -0,0 +1,10 @@
+(function(){
+  "use strict";
+
+  var module = angular.module( 'app', ['ngAnimate' ])
+
+  module.controller('AppCtrl', function($scope){
+    console.log('AppCtrl!');
+  });
+
+})();
---
+++ source/all.js
@@ -0,0 +1,14 @@
+//= include '../bower_components/angular/angular.js'
+//= include '../bower_components/angular-animate/angular-animate.js'
+
+//= include '_common/**/*.ngt'
+//= include '_common/**/*.js' '!_common/**/*spec.js'
+
+//= include '_app/**/*.ngt'
+//= include '_app/**/*.js' '!_app/**/*spec.js'
+
+/* 
+  Any code defined after the include directives will 
+  be appended at the end of the file unless 
+  "include_self" is defined. 
+*/
